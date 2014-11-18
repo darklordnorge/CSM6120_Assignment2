fe28e00b414c75de9d125d7cb23bddc78586f722
darklordnorge <romteknolog@gmail.com>
2014-11-18 13:30:26 +0000

    Finish nextStep method in Graph class
    This method has been finished and tested

--- src/search_Algortihms/Graph.java
+++ src/search_Algortihms/Graph.java
@@ -23,20 +23,44 @@ public class Graph {
      * horizontally and vertically.
      * To make the process easier simple numbers identifiers 
      * are assigned to the possible
-     * tiles in the puzzle. These numbers are :
+     * tiles in the puzzle. These numbers represent indices in the arraylist
+     * and are :
      * 0 1 2
      * 3 4 5
      * 6 7 8
-     * 
+     * This method will always choose the next smallest index to switch with
      * @param s The state on which the next step will be based 
      */
     public void nextStep(State s){
-        int emptyTile; //place of the empty tile
+        int emptyTile; //index of the empty tile
         emptyTile = s.returnIndex(0);
        
-        if(emptyTile == 2){
+        if(emptyTile == 0){
             s.changeTiles(1, emptyTile);
-            s.printArray();
+        }
+        else if(emptyTile == 1){
+            s.changeTiles(0, emptyTile);
+        }
+         else if(emptyTile == 2){
+            s.changeTiles(1, emptyTile);
+        }
+         else if(emptyTile == 3){
+            s.changeTiles(0, emptyTile);
+        }
+         else if(emptyTile == 4){
+            s.changeTiles(1, emptyTile);
+        }
+         else if(emptyTile == 5){
+            s.changeTiles(2, emptyTile);
+        }
+         else if(emptyTile == 6){
+            s.changeTiles(3, emptyTile);
+        }
+         else if(emptyTile == 7){
+            s.changeTiles(4, emptyTile);
+        }
+         else if(emptyTile == 8){
+            s.changeTiles(5, emptyTile);
         }
     }
 }
