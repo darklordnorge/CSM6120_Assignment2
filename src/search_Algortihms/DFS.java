e2afd509c63a5587d4aa286e6d8660ab9591f6a6
emdarklordnorge/em <emromteknolog@gmail.com/em>
2014-11-23 20:34:25 +0000

    Work on DFS algorithm
    More work has been done on the dfs algorithm, it will now find solutions to all problems,
    however it needs to much time, this will be worked on

--- src/search_Algortihms/DFS.java
+++ src/search_Algortihms/DFS.java
@@ -1,5 +1,5 @@
-
 package search_Algortihms;
+
 import SearchTree.*;
 import csm6120_assignment2.State;
 import java.util.ArrayList;
@@ -7,84 +7,80 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-
 /**
  *
  * @author stefan
  */
 public class DFS {
+
     Graph tree;
     TreeNode node, root;
     Stack<TreeNode> searchStack, exploredStack;
     int pathcost;
     ArrayList expanded;
-    
-    public DFS(){
+
+    public DFS() {
         tree = new Graph();
         searchStack = new Stack();
         exploredStack = new Stack();
         pathcost = 0;
         expanded = new ArrayList<String>();
     }
-    
-    public void dfs(State start, State goal){
+
+    public void dfs(State start, State goal) {
         System.out.println("Using Depth-First Search");
         root = new TreeNode(start);
         /*
-        check the root for being the goal state
-        */
-        if(root.returnState().compare(goal)){
-            System.out.println("Solution has been found.\n Path cost: " 
-                    +  pathcost);
+         check the root for being the goal state
+         */
+        if (root.returnState().compare(goal)) {
+            System.out.println("Solution has been found.\n Path cost: "
+                    + pathcost);
             System.out.println("Current node: ");
             root.returnState().printArray();
             System.out.println("Goal state: ");
             goal.printArray();
         }
-         searchStack.add(root);
-         
-         while(searchStack.size() > 0){
-             node = searchStack.pop();
-             
-             /*
+        searchStack.add(root);
+
+        while (searchStack.size() > 0) {
+            node = searchStack.pop();
+
+            /*
              check the current node for being the goal node
              */
-             if(node.returnState().compare(goal)){
-                System.out.println("Solution has been found.\n Path cost: " 
-                    +  pathcost);
+            if (node.returnState().compare(goal)) {
+                System.out.println("Solution has been found.\n Path cost: "
+                        + pathcost);
                 System.out.println("Current node: ");
                 node.returnState().printArray();
                 System.out.println("Goal state: ");
                 goal.printArray();
-                 break;
-             }
-             //generate the next level based on that node and add
-             tree.nextStep(node);
-             
-             /*
-             Add the current node to a an ArrayList of expanded nodes
-             */
-             expanded.add(node.returnState().returnString());
-             
-             
-             /*
+                break;
+            }
+            //generate the next level based on that node and add
+            tree.nextStep(node);
+
+            /*
              add the child of the current node and all its siblings to the queue
              */
-             while(node.hasChildren() != true){ 
-                 String s = node.peekChield().returnState().returnString();
-                 if(expanded.contains(s) == false){
-                   searchStack.add(node.returnChield());  
-                 }
-                 else{
-                     node.removeFirstChield();
-                 }
-                
-                 
-                 
-             }
-             exploredStack.add(node);
-             pathcost++;
-             node = searchStack.pop();
+            while (node.hasChildren() != true) {
+                /*
+                 Add the current node to a an ArrayList of expanded nodes
+                 */
+                if (expanded.contains(node.returnState().returnString()) == false) {
+                    expanded.add(node.returnState().returnString());
+                }
+                String s = node.peekChield().returnState().returnString();
+                if (expanded.contains(s) == false) {
+                    expanded.add(s);
+                    searchStack.push(node.returnChield());
+                } else {
+                    node.removeFirstChield();
+                }
+            }
+            exploredStack.add(node);
+            pathcost++;
         }
     }
 }
