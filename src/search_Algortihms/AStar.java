cc6c2d8d8ba73e9915624dda46b0473a02731270
Stefan Klaus <romteknolog@gmail.com>
2014-11-28 15:15:11 +0000

    Add new method to A* class
    This new method is used to add a node to the search queue. This method
    calculated the Manhattan distance for every child of the, current, node
    and only adds those which calculated Manhattan distance is lower than
    the original calculated one to the search queue.
    JavaDoc for this method has been written.
    
    Testing shows that the algorithm performs perfectly for the first 2 test
    scenarios, where the estimated path cost and the actual path cost
    matches. Only on the 3 test scenario does it underestimate the actual
    path cost

--- src/search_Algortihms/AStar.java
+++ src/search_Algortihms/AStar.java
@@ -15,6 +15,7 @@ import java.util.Queue;
 
 /**
  * A* algorithm class
+ *
  * @author Stefan
  */
 public class AStar {
@@ -27,7 +28,8 @@ public class AStar {
     private StateComparator comparator;
     private ArrayList<String> expanded;
     private ManhattanDistance md;
-    
+    private int manhattanDistanceSum;
+
     /**
      * A* class constructor
      */
@@ -39,24 +41,23 @@ public class AStar {
         comparator = new StateComparator();
         this.searchQueue = new PriorityQueue(500, comparator);
         expanded = new ArrayList<String>();
-        
+        manhattanDistanceSum = 0;
+
     }
-    
+
     /**
      * A* algorithm
-     * 
+     *
      * @param start The start State
-     * @param goal  The goal State 
+     * @param goal The goal State
      */
     public void astar(State start, State goal) {
         System.out.println("Using A* Search");
         /*
-        initialise ManhattanDistance
-        */
+         initialise ManhattanDistance
+         */
         this.md = new ManhattanDistance(start, goal);
-        
-        int manDist;
-        
+
         root = new TreeNode(start);
         /*
          check the root for being the goal state
@@ -70,10 +71,10 @@ public class AStar {
             goal.printArray();
         }
         searchQueue.add(root);
-        
-        manDist = md.calcManhattanDistance(root.returnState(), goal);
-        System.out.println("Manhatten distance is: " + manDist);
-        
+
+        manhattanDistanceSum = md.calcManhattanDistance(root.returnState(), goal);
+        System.out.println("Manhatten distance is: " + manhattanDistanceSum);
+
 
         /*
          Iterate while the search queue is not empty
@@ -95,61 +96,72 @@ public class AStar {
             }
             //generate the next level based on that node and add
             tree.nextStep(node);
-
+            this.addNode(node, goal);
             /*
              add the child of the current node and all its siblings to the queue
              */
-            while (node.childrenIsEmpty() != true) {
-                /*
-                 Add the current node to a an ArrayList of expanded nodes
-                 */
-                if (expanded.contains(node.returnState().getStringtoString()) == false) {
-                    expanded.add(node.returnState().getStringtoString());
-                }
-                String s = node.peekChield().returnState().getStringtoString();
-                if (expanded.contains(s) == false && pathcost < manDist) {
-                    expanded.add(s);
-                    searchQueue.add(node.pollChield());
-                } else {
-                    node.removeFirstChield();
-                }
-            }
+//            while (node.childrenIsEmpty() != true) {
+//                /*
+//                 Add the current node to a an ArrayList of expanded nodes
+//                 */
+//                if (expanded.contains(node.returnState().getStringtoString()) == false) {
+//                    expanded.add(node.returnState().getStringtoString());
+//                }
+//                String s = node.peekChield().returnState().getStringtoString();
+//                if (expanded.contains(s) == false && pathcost < manhattanDistanceSum) {
+//                    expanded.add(s);
+//                    searchQueue.add(node.pollChield());
+//                } else {
+//                    node.removeFirstChield();
+//                }
+//            }
 
             /*
              add the current node to the explored node and 
              update path cost
              */
             exploredQueue.add(node);
-            pathcost++;
+            pathcost = exploredQueue.size();
         }
     }
+    
     /**
-     * 1 0 3
-     * 4 8 5
-     * 9 7 2
+     * Method to add a new node the the search queue.
+     * This method calculates the Manhattan Distance for every child in the 
+     * current node and only add them to the search queue if the 
+     * Manhattan Distance is less than the original calculated one.
      * 
-     * @param s
-     * @param goal 
+     * @param current       The current node
+     * @param goal          The goal node, use for the Manhattan Distance
+     *                      calculation
      */
-//    public void calcManhattenDistance(State s, State goal){
-//        int sIndex, goalIndex, temp = 0;
-//        for(int i = 0;i<goal.getArraySize();i++){
-//            sIndex = s.returnIndex(i); //eg.3
-//            goalIndex = goal.returnIndex(i); //eg.5
-//            
-////            if(sIndex != goalIndex){
-////                temp = goal.returnIndex(sIndex);
-////                maxDistance += (sIndex - goalIndex) + (sIndex - temp);
-////            }
-//            
-//            if(goalIndex - sIndex < 0){
-//                maxDistance += sIndex + goalIndex;
-//            }
-//            if(goalIndex - sIndex > 0){
-//               maxDistance += goalIndex - sIndex; 
-//            }
-//           
-//        }
-//        System.out.println("Manhatten distance is: " + maxDistance);
-//    }
+    public void addNode(TreeNode current, State goal) {
+        int currentManhattanDistance;
+        String s;
+        TreeNode temp;
+
+        while (current.childrenIsEmpty() != true) {
+            /*
+             Add the current node to the list of expanded nodes
+             */
+            if (expanded.contains(current.returnState().getStringtoString()) == false) {
+                expanded.add(current.returnState().getStringtoString());
+            }
+
+            s = current.peekChield().returnState().getStringtoString();
+
+            if (expanded.contains(s) == false) {
+                temp = current.pollChield();
+                currentManhattanDistance = md.
+                        calcManhattanDistance(temp.returnState(), goal);
+
+                if (currentManhattanDistance < manhattanDistanceSum) {
+                    expanded.add(s);
+                    searchQueue.add(temp);
+                }
+            } else {
+                current.removeFirstChield();
+            }
+        }
+    }
 }
